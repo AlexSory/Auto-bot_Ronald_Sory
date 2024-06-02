ee4d889ab19e5a9c10a70f53de1dc0e91a0ec7d9
AlexSory <soryalexandre7@gmail.com>
2024-06-02 18:55:27 +0000

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -24,7 +24,7 @@ module.exports.run = async function({
   try {
     const {
       data
-    } = await axios.get(`https://soyeon-api.onrender.com/api?prompt=${encodeURIComponent(input)}`);
+    } = await axios.get(`https://openaikey-x20f.onrender.com/api}`);
     const response = data.response;
     api.sendMessage(response + '\n══════════════════\nhttps://www.facebook.com/sory.ronald.alexandre', event.threadID, event.messageID);
   } catch (error) {
